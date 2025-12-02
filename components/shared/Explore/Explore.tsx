ae450a43ef575e84cebdc8a28b146b9e9ed0a1bc
Max <kvsam7395@gmail.com>
2025-12-02 20:12:51 +0200

    feat(explore) - init explore

---
+++ components/shared/Explore/Explore.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Explore() {
+    return (
+        <div>
+            <h1>Explore Marketplace</h1>
+            <div></div>
+        </div>
+    );
+}
